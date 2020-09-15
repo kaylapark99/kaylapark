b4096324151c6e8cf723f240a1605e79b9e305e8
Kayla Park <51247286+kaylapark99@users.noreply.github.com>
2020-09-15 14:41:24 -0400

    Add files via upload
    Adding HTML/CSS/Javascript

---
+++ scripts.js
@@ -0,0 +1,69 @@
+var words = ["of beautiful user experiences", "of effective/efficient code", "of some dope art", "of various spotify playlists"];
+let i = 0, timer = 0;
+
+var leftCode = document.getElementById("leftCode"), rightArt = document.getElementById("rightArt");
+
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function() {
+		if (word.length > 0) {
+			document.getElementById('word').innerHTML += word.shift();
+        } 
+        else {
+			deletingEffect();
+			return false;
+		};
+		timer = setTimeout(loopTyping, 225);
+	};
+	loopTyping();
+};
+
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function() {
+		if (word.length > 0) {
+			word.pop();
+			document.getElementById('word').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 100);
+	};
+	loopDeleting();
+};
+
+typingEffect();
+
+[leftCode, rightArt].forEach(function(element){
+	var codeDrawing = document.getElementById("code-drawing"), artDrawing = document.getElementById("art-drawing"),
+	codeLink = document.getElementById("code-link"), artLink = document.getElementById("art-link");
+    element.addEventListener("mouseover", function() {
+        element.style["transition-duration"] = "0.4s";
+        element.classList.add("creation-hover");
+        if(element.isSameNode(leftCode)) {
+			codeDrawing.src = "media/leftHoverInverse.png";
+			codeLink.style.color = "#8ae7ff";
+        }
+        else {
+			artDrawing.src = "media/rightHover.png";
+			artLink.style.color = "#8ae7ff";
+        }
+    });
+    element.addEventListener("mouseout", function() {
+        element.classList.remove("creation-hover");
+        if(element.isSameNode(leftCode)) {
+			codeDrawing.src = "media/leftinverse.png";
+			codeLink.style.color = "white";
+        }
+        else {
+			artDrawing.src = "media/right.png";
+			artLink.style.color = "black";
+        }
+    });
+});
---
+++ scriptsNav.js
@@ -0,0 +1,37 @@
+function openNav() {
+    document.getElementById("toggleNav").classList.add("overlay-open");
+    document.getElementById("toggleNav").classList.remove("overlay-closed");
+    document.getElementById("open-button").style.display = "none";
+    document.getElementById("close-button").style.display = "inline";
+}
+
+function closeNav() {
+    document.getElementById("open-button").style.display = "inline";
+    document.getElementById("close-button").style.display = "none";
+    document.getElementById("toggleNav").classList.remove("overlay-open");
+    document.getElementById("toggleNav").classList.add("overlay-closed");
+}
+
+function checkCloseNav() {
+    if (document.URL.includes("index.html")) {
+        closeNav();
+    }
+}
+
+function openArt() {
+    window.location = 'art.html';
+}
+
+function openCode() {
+    window.location = 'code.html';
+}
+
+function openAbout() {
+    window.location = 'about.html';
+}
+
+function displayNav(item, x, opc) {
+    item.style.transition = '0.9s';
+    item.style.opacity = opc;
+    item.innerHTML = x;
+}
